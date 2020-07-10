17d4c1efa3133e09801ed765bd7564380eaf87b7
jellyrain <840519987@qq.com>
2020-07-10 23:42:31 +0800

    my

--- js/music_data.js
+++ js/music_data.js
@@ -6,6 +6,18 @@ var data_music = [
                 "music_img" : "image/maplelove..jpg"
         },
         {
+                "music" : "Cetus",
+                "name" : "Lensko",
+                "music_src" : "music/Lensko - Cetus.mp3",
+                "music_img" : "image/Cetus.jpg"
+        },
+        {
+                "music" : "妖刀",
+                "name" : "Frost Fragment",
+                "music_src" : "music/Frost Fragment - 妖刀.flac",
+                "music_img" : "image/妖刀.jpg"
+        },
+        {
                 "music" : "Conclusion",
                 "name" : "PHOENIX Project",
                 "music_src" : "music/PHOENIX Project - Conclusion.mp3",
@@ -28,6 +40,18 @@ var data_music = [
                 "name" : "築山さえ",
                 "music_src" : "music/築山さえ - Lonely Rabbit.flac",
                 "music_img" : "image/Lonely_Rabbit.jpg"
+        },
+        {
+                "music" : "S.O.S.",
+                "name" : "3L",
+                "music_src" : "music/3L - S.O.S.flac",
+                "music_img" : "image/S.O.S..jpg"
+        },
+        {
+                "music" : "NECRO EFFECT",
+                "name" : "坂上なち",
+                "music_src" : "music/坂上なち - NECRO EFFECT.flac",
+                "music_img" : "image/NECRO EFFECT.jpg"
         }
 ]
 

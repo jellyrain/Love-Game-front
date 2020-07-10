efa81cfa36138d6ccacaa0e63abb1355182349fa
jellyrain <840519987@qq.com>
2020-07-10 23:10:37 +0800

    my

--- js/music.js
+++ js/music.js
@@ -8,42 +8,12 @@ var music_totalProgress = document.getElementById("music_totalProgress");
 var music_currentProgress = document.getElementById("music_currentProgress");
 var music_volume_totalProgress = document.getElementById("volume_totalProgress");
 var music_volume_currentProgress = document.getElementById("volume_currentProgress");
-var music_json = [
-    {
-            "music" : "maplelove.",
-            "name" : "Studio 'Syrup Comfiture'",
-            "music_src" : "music/Studio ＂Syrup Comfiture＂ - maplelove.flac",
-            "music_img" : "image/maplelove..jpg"
-    },
-    {
-            "music" : "Conclusion",
-            "name" : "PHOENIX Project",
-            "music_src" : "music/PHOENIX Project - Conclusion.mp3",
-            "music_img" : "image/Conclusion.jpg"
-    },
-    {
-            "music" : "nostalgia",
-            "name" : "流派未階堂",
-            "music_src" : "music/流派未階堂 - nostalgia.flac",
-            "music_img" : "image/nostalgia.jpg"
-    },
-    {
-            "music" : "Leaving for...",
-            "name" : "MisteryCircle",
-            "music_src" : "music/MisteryCircle - Leaving for.mp3",
-            "music_img" : "image/Leaving_for....jpg"
-    },
-    {
-            "music" : "Lonely Rabbit",
-            "name" : "築山さえ",
-            "music_src" : "music/築山さえ - Lonely Rabbit.flac",
-            "music_img" : "image/Lonely_Rabbit.jpg"
-    }
-];
+var music_json;
 var music_i = 0;
 var music_max;
 
 window.onload = function(){
+        music_json = music_data();
         music_max = music_json.length;
         loading(music_i);
         music.play();
